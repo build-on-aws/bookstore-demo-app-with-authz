34d03671b3935cf1679d69cff2577b3251cd2186
Wojtek Gawroński <wojgaw@amazon.com>
2023-11-22 13:41:59 +0100

    Step 1: Basic authorization (pre RBAC)
    - Admins see everything.
    - Everyone else sees nothing.

--- backend/product-service/get_products.py
+++ backend/product-service/get_products.py
@@ -36,8 +36,8 @@ def lambda_handler(event: dict, context: LambdaContext):
 
     logger.info(f"User info: {user_info}")
 
-    # No authorization.
-    product_list = books["books"]
+    # Handle authorization decision.
+    product_list = handle_is_authorized(user_info)
 
     logger.info(f"Successfully returning product list - Count: {len(product_list)}")
 
@@ -46,3 +46,66 @@ def lambda_handler(event: dict, context: LambdaContext):
         "headers": HEADERS,
         "body": json.dumps({"products": product_list}),
     }
+
+
+def handle_is_authorized(user_info):
+    # Construct the authorization request.
+    authz_request = construct_authz_request(user_info)
+    logger.info(f"Authorization request: {authz_request}")
+
+    # Make the `is_authorized` call to Amazon Verified Permissions.
+    response = verified_permissions_client.is_authorized(**authz_request)
+    logger.info(f"Authorization response: {response}")
+
+    # Determine which product list to return.
+    return determine_product_list(response, user_info)
+
+
+def construct_authz_request(user_info):
+    entities = [
+        {
+            "identifier": {
+                "entityType": "Bookstore::User",
+                "entityId": user_info["username"]
+            },
+            "attributes": {},
+            "parents": [
+                {
+                    "entityType": "Bookstore::Role",
+                    "entityId": user_info["role"]
+                }
+            ]
+        }
+    ]
+
+    resource = {
+        "entityType": "Bookstore::Book",
+        "entityId": "*"
+    }
+
+    action_id = "View"
+
+    return {
+        "policyStoreId": os.environ.get("POLICY_STORE_ID"),
+        "principal": {
+            "entityType": "Bookstore::User",
+            "entityId": user_info["username"]
+        },
+        "action": {
+            "actionType": "Bookstore::Action",
+            "actionId": action_id
+        },
+        "resource": resource,
+        "entities": {"entityList": entities},
+        "context": {"contextMap": {}}
+    }
+
+
+def determine_product_list(response, user_info):
+    if "decision" in response:
+        if response["decision"] == "ALLOW":
+            return books["books"]
+        elif response["decision"] == "DENY":
+            return []  # Return an empty list for denied access.
+
+    return []
