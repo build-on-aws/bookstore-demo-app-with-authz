0714e51068e908636961d0a82fa86181e259a7db
Wojtek Gawroński <wojgaw@amazon.com>
2023-11-22 14:54:59 +0100

    Step 5: Granular Permissions
    - Here you can see two new policies for Publishers, that will be handled with `batch_is_authorized` call.
      - Dante will see books that he owns (1x) and other book, granted based on the ID - that is owned by William.
      - William will see only the books that he owns though.

--- backend/product-service/get_products.py
+++ backend/product-service/get_products.py
@@ -19,6 +19,9 @@ HEADERS = {
 }
 
 POLICIES = {
+    "PUBLISHERS_VIEW": "Allows publishers to list books they have published",
+    "PUBLISHER_ACCESS_TO_ONE_BOOK": "Allows specific Publisher to list a specific book",
+    "PREMIUM_OFFERS": "Allows customers with specific value for yearsAsMember attribute to list premium offers",
     "NO_PREMIUM_OFFERS": "Denies customers with specific value for yearsAsMember attribute to list premium offers",
 }
 
@@ -41,7 +44,9 @@ def lambda_handler(event: dict, context: LambdaContext):
     logger.info(f"User info: {user_info}")
 
     # Handle authorization decision.
-    product_list = handle_is_authorized(user_info)
+    product_list = (
+        handle_is_authorized(user_info) if user_info["role"] != "Publisher" else handle_batch_is_authorized(user_info)
+    )
 
     logger.info(f"Successfully returning product list - Count: {len(product_list)}")
 
@@ -134,6 +139,13 @@ def construct_authz_request(user_info):
 def determine_product_list(response, user_info):
     if "decision" in response:
         if response["decision"] == "ALLOW":
+            policy_description = get_policy_description(response)
+
+            if user_info["role"] == "Publisher" and policy_description == POLICIES["PUBLISHERS_VIEW"]:
+                # Return books published by this publisher.
+                return [book for book in books["books"] if book["publisher"] == user_info["username"]]
+
+            # If allowed and not a publisher, return all books including premium offers.
             return books["books"]
         elif response["decision"] == "DENY":
             # Handle the deny decision for customers that should not see premium offers.
@@ -147,3 +159,134 @@ def determine_product_list(response, user_info):
             return []  # Return an empty list for denied access.
 
     return []
+
+
+def handle_batch_is_authorized(user_info):
+    # Construct the batch authorization request.
+    batch_authz_request = construct_batch_authz_request(user_info)
+    logger.info(f"Batch authorization request: {batch_authz_request}")
+
+    # Make `batch_is_authorized` call to Amazon Verified Permissions.
+    responses = verified_permissions_client.batch_is_authorized(**batch_authz_request)
+    logger.info(f"Batch authorization response: {responses}")
+
+    return determine_product_list_for_publisher(responses, user_info)
+
+
+def construct_batch_authz_request(user_info):
+    evaluated_user = {
+        "identifier": {
+            "entityType": "Bookstore::User",
+            "entityId": user_info["username"]
+        },
+        "attributes": {},
+        "parents": [
+            {
+                "entityType": "Bookstore::Role",
+                "entityId": user_info["role"]
+            }
+        ]
+    }
+
+    # Always one action ID in this case.
+    action_id = "View"
+
+    # Set `region` attribute in the context based on the specific user attributes.
+    region = user_info.get("region", "Unknown")
+    context_map = {"region": {"string": region}}
+
+    book_owned_by_william = "em1oadaa-b22k-4ea8-kk33-f6m217604o3m"
+    book_owned_by_dante = "fn2padaa-c33l-4ea8-ll44-g7n217604p4n"
+
+    batch_request = {
+        "policyStoreId": os.environ.get("POLICY_STORE_ID"),
+        "entities": {
+            "entityList": [
+                evaluated_user,
+
+                # Here we add books that will be evaluated against publishers.
+                # In our example, each of two Publishers - Dante and William - have just one book.
+                # However, we have added new policy, that Dante can list a specific book owned by William.
+
+                get_publisher_book_entity(book_owned_by_william, "William"),
+                get_publisher_book_entity(book_owned_by_dante, "Dante")
+            ]
+        },
+        "requests": [
+            {
+                "principal": {
+                    "entityType": "Bookstore::User",
+                    "entityId": user_info["username"]
+                },
+                "action": {
+                    "actionType": "Bookstore::Action",
+                    "actionId": action_id
+                },
+                "resource": {
+                    "entityType": "Bookstore::Book",
+                    "entityId": book_owned_by_william
+                },
+                "context": {
+                    "contextMap": context_map
+                }
+            },
+            {
+                "principal": {
+                    "entityType": "Bookstore::User",
+                    "entityId": user_info["username"]
+                },
+                "action": {
+                    "actionType": "Bookstore::Action",
+                    "actionId": action_id
+                },
+                "resource": {
+                    "entityType": "Bookstore::Book",
+                    "entityId": book_owned_by_dante
+                },
+                "context": {
+                    "contextMap": context_map
+                }
+            }
+        ]
+    }
+
+    return batch_request
+
+
+def get_publisher_book_entity(book_id, username):
+    return {
+        "identifier": {
+            "entityType": "Bookstore::Book",
+            "entityId": book_id
+        },
+        "attributes": {
+            "owner": {
+                "entityIdentifier": {
+                    "entityType": "Bookstore::User",
+                    "entityId": username
+                }
+            }
+        },
+        "parents": []
+    }
+
+
+def determine_product_list_for_publisher(responses, user_info):
+    allowed_books = []
+
+    for response in responses.get("results", []):
+        if response.get("decision") == "ALLOW":
+            policy_description = get_policy_description(response)
+
+            # Check if the policy allows the publisher to list the books they have published.
+            # If not, check the other policy, if that allows a specific user to list a specific book.
+            if policy_description == POLICIES["PUBLISHERS_VIEW"]:
+                allowed_books.extend([book for book in books["books"] if book["publisher"] == user_info["username"]])
+            elif policy_description == POLICIES["PUBLISHER_ACCESS_TO_ONE_BOOK"]:
+                book_id = response["request"]["resource"]["entityId"]
+                allowed_books.extend([book for book in books["books"] if book["id"] == book_id])
+
+    # Remove duplicates, if any.
+    allowed_books = [dict(t) for t in {tuple(book.items()) for book in allowed_books}]
+
+    return allowed_books
