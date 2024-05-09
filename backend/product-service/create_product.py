1b97b5f5e8db8d603a943b53f7be5bf035b60566
Wojtek Gawroński <wojgaw@amazon.com>
2024-05-09 20:55:46 +0200

    Adding new routes, Cognito groups, and preparation for enhanced version.

---
+++ backend/product-service/create_product.py
@@ -0,0 +1,37 @@
+import json
+import os
+from aws_lambda_powertools import Logger, Tracer
+
+logger = Logger()
+tracer = Tracer()
+
+with open("database.json", "r") as database:
+    books = json.load(database)
+
+HEADERS = {
+    "Access-Control-Allow-Origin": os.environ.get("ALLOWED_ORIGIN"),
+    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization",
+    "Access-Control-Allow-Methods": "OPTIONS,GET",
+    "Access-Control-Allow-Credentials": True,
+}
+
+
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event, context):
+    """
+    Return single product based on path parameter.
+    """
+    path_params = event["pathParameters"]
+    book_id = path_params.get("book_id")
+
+    logger.debug(f"Retrieving book ID: {book_id}")
+
+    product = next(
+        (book for book in books["books"] if book["id"] == book_id), None
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": HEADERS,
+        "body": json.dumps({"product": product}),
+    }
